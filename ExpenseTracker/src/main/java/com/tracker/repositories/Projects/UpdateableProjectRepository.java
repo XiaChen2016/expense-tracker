d130dcd3381cfeb17d1964f383a591878f491569
Xia Chen <chen.xia@uwlax.edu>
2017-02-09 11:58:22 -0600

    same

--- ExpenseTracker/src/main/java/com/tracker/repositories/Projects/UpdateableProjectRepository.java
+++
@@ -1,7 +0,0 @@
-package com.tracker.repositories.projects;
-
-import com.tracker.domain.project.Project;
-
-public interface UpdateableProjectRepository {
-	public void update( Project project );
-}
